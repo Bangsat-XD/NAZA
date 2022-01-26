0f20c11f70d4b92f0189facdd46903ad45b14b0c
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2022-01-26 21:41:12 +0700

    Update Naza.py

--- Naza.py
+++ Naza.py
@@ -123,7 +123,7 @@ def country():
     
 def choose_country():
     cc = raw_input("\n%s[%s•%s] %sChoose : "%(k,p,k,p))
-    if cc = " ":
+    if cc = "":
         print((k+"\n["+p+"!"+k+"]"+p+" Fill In The Correct"))
     elif cc = "1","01":
         os.system("rm -rf country.txt")
@@ -317,27 +317,27 @@ def menu():
     choose_menu()
 
 def choose_menu():
-	r=input(k+"\n["+p+"•"+k+"]"+p+"Choose : ")
-	if r=="":
+	r = raw_input(k+"\n["+p+"•"+k+"]"+p+"Choose : ")
+	if r== "":
 		print((k+"["+p+"!"+k+"]"+p+" Fill In The Correct"))
 		menu()
-	elif r=="1":
+	elif r == "1":
 		publik()
-	elif r=="2":
+	elif r == "2":
 		follow()
-	elif r=="3":
+	elif r == "3":
 		likers()
-	elif r=="4":
+	elif r == "4":
 		random_numbers()
-	elif r=="5":
+	elif r == "5":
 		random_email()
-	elif r=="6":
+	elif r == "6":
 		target()
-	elif r=="7":
+	elif r == "7":
 		ress()
-	elif r=="8":
+	elif r == "8":
 		menu_user_agent()
-	elif r=="0":
+	elif r == "0":
 		try:
 			jalan(k+"\n["+p+"+"+k+"]"+p+" Mantap Tod...")
 			os.system("rm -rf login.txt")
